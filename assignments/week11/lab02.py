4c260c669f7baf2ffc5396b6ba187a93ea4c02d5
Chinawut Sansiri <chinawut.s@ku.th>
2025-10-07 14:45:35 +0700

    lab week11

---
+++ assignments/week11/lab02.py
@@ -0,0 +1,56 @@
+"""
+Create a grade processing system with the following requirements:
+
+A global variable passing_grade = 50
+
+(1) A function input_students(num_students) that:
+
+- Creates and returns a list of dictionaries
+- Each dictionary contains: {'name': str, 'scores': [list of 3 scores]}
+
+(2) A function calculate_averages(students) that:
+
+- Uses nested loops to calculate each student's average
+- Adds an 'average' key to each student dictionary
+- Modifies the original list (demonstrate list mutability)
+
+(3) A function display_results(students) that:
+
+- Loops through students
+- Uses the global passing_grade to determine pass/fail
+- Prints each student's name, average, and status (PASS/FAIL)
+"""
+
+passing_grade = 50
+
+def input_students(num_students=0):
+    students = [
+        {
+            'name' : 'Patoo',
+            'scores' : [47, 64, 53],
+        },
+        {'name': 'Manee', 'scores': [75, 84, 92]}
+    ]
+    return students
+
+def calculate_averages(students):
+    for student in students:
+        sum = 0
+        for score in student['scores']:
+            sum = sum + score
+        student['average'] = sum / 3
+
+    return students
+
+def display_results(students):
+    for student in students:
+        print("Name:", student['name'])
+        print("Average: %.1f "% (student['average']))
+        if student['average'] > passing_grade:
+            print("Status: PASS")
+        else:
+            print("Status: FAIL")
+
+students_list = input_students()
+students_list = calculate_averages(students_list)
+display_results(students_list)
